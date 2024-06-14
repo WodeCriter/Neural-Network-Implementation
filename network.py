53326b27d83ed6b8a172a44f6c9e3892c55e277a
Daniel <danielpbea@gmail.com>
2024-06-14 21:10:50 +0300

    feat: add backpropagation method to Network class
    The `backpropagation` method is added to the `Network` class to calculate the gradients of the cost function with respect to the biases and weights. It performs a feedforward pass to calculate the activations and z vectors for each layer, and then performs a backward pass to propagate the error and calculate the gradients.

--- network.py
+++ network.py
@@ -17,9 +17,10 @@ class Network:
 
         #activation function and its derivative for all layers exept output layer (for now)
         #TODO(1) level 1 : instead of a single function, we can use a list of function pointers (one function for each layer)
-        #TODO(1) level 2 : we can have a list of vectors (one vector for each layer) to control the activation function of *each neuron*
         self.__activation_func = self.sigmoid
         self.__activation_prime = self.sigmoid_prime
+        
+        self.__cost_derivative = self.quadratic_cost_derivative
 
     #given the input a, return the output of the network (for now, using sigmoid only)
     def feedforward(self, a):
@@ -29,7 +30,46 @@ class Network:
             a = self.__activation_func(np.dot(w, a) + b)
         return a
 
-    
+
+    #TODO(1)
+    def backpropagation(self, x, y):
+        #initialize the lists for the gradients of the cost function
+        #with respect to the biases and weights
+        nabla_b = [np.zeros(b.shape) for b in self.__biases]
+        nabla_w = [np.zeros(w.shape) for w in self.__weights]
+
+        #feedforward
+        #TODO(1)
+        activation = x
+        #list to store all the activations, layer by layer
+        activations = [x]
+        #list to store all the z vectors, layer by layer
+        zs = []
+        for b, w in zip(self.__biases, self.__weights):
+            z = np.dot(w, activation) + b
+            zs.append(z)
+            #TODO(1)
+            activation = self.__activation_func(z)
+            activations.append(activation)
+
+        #backward pass
+        #calculate the error in the output layer
+        delta = self.__cost_derivative(activations[-1], y) * self.__activation_prime(zs[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+
+        #propagate the error to the previous layers
+        for l in range(2, self.__num_layers):
+            z = zs[-l]
+            #TODO(1)
+            ap = self.__activation_prime(z)
+            delta = np.dot(self.__weights[-l + 1].transpose(), delta) * ap
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, activations[-l - 1].transpose())
+
+        return nabla_b, nabla_w
+
+
 
     #looks scary, but the only thing it does is setting the activation function
     #input: string with the name of the activation function
@@ -87,3 +127,15 @@ class Network:
     
     def linear_prime(self, z):
         return 1
+    
+
+
+    #loss function derivatives
+
+    #--quadratic cost--
+    def quadratic_cost_derivative(self, output_activations, y):
+        return (output_activations - y)
+    
+    #--huber loss--
+    def huber_loss_derivative(self, output_activations, y):
+        return np.where(np.abs(output_activations - y) <= 1, output_activations - y, np.sign(output_activations - y))
